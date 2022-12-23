ba220b2245065b952eb74cb1e5cc270828a8399d
Cake <cakemancerr@gmail.com>
2022-12-23 18:49:31 +0100

    styles: add tailwind animations

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,6 +8,20 @@ module.exports = {
       fontFamily: {
         sans: ["Unbounded", ...defaultTheme.fontFamily.sans],
       },
+      keyframes: {
+        menu: {
+          "0%": { transform: "scaleX(0)" },
+          "100%": { transform: "scaleX(1)" },
+        },
+        "menu-reverse": {
+          "0%": { transform: "scaleX(1)" },
+          "100%": { transform: "scaleX(0)" },
+        },
+      },
+      animation: {
+        open: "menu 0.3s ease-in-out forwards",
+        "open-reverse": "menu-reverse 0.3s ease-in-out forwards",
+      },
     },
   },
   plugins: [],
