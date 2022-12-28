4b377d705698ef63799cef04f387531f47a08618
Cake <cakemancerr@gmail.com>
2022-12-28 18:57:52 +0100

    chore: add astro compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
 import { defineConfig } from "astro/config";
-
+import compress from "astro-compress";
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   integrations: [
+    compress(),
     tailwind({
-      config: { applyBaseStyles: false },
+      config: {
+        applyBaseStyles: false,
+      },
     }),
   ],
 });
