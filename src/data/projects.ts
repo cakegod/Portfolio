661bb530cbabfcbf6a30e4cf7e807f032f0cf589
Cake <cakemancerr@gmail.com>
2022-12-30 18:09:21 +0100

    feat: add blog project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,15 +1,34 @@
 import shopping from "/shopping.webp";
 import memory from "/memory.webp";
+import blog from "/blog.webp";
 import type { ProjectProps } from "@customTypes/projects";
 
 const projectsData: ProjectProps[] = [
   {
+    title: "Blog API",
+    details:
+      "A full-stack blog application made with Next JS for the front-end and Express JS for the back-end.",
+    image: blog,
+    link: "https://blog-cakegod.vercel.app/blog",
+    list: [
+      "Created a <strong>REST API</strong> as the back-end with Express JS.",
+      "Implemented <strong>MongoDB</strong> as the database.",
+    ],
+    tags: [
+      { name: "Tailwind", color: "emerald" },
+      { name: "React", color: "blue" },
+      { name: "Zustand", color: "red" },
+    ],
+  },
+  {
     title: "Shopping Cart",
-    details: "A front end project made with React and Next JS.",
+    details:
+      "An e-commerce front-end application, made with React and Next JS.",
     image: shopping,
     link: "https://shopping-cart-cakegod.vercel.app/shop",
     list: [
       "Utilized <strong>Next.js</strong> for routing and optimization of images and fonts.",
+      "Implemented <strong>React context</strong> to avoid prop drilling.",
     ],
     tags: [
       { name: "Tailwind", color: "emerald" },
@@ -19,27 +38,18 @@ const projectsData: ProjectProps[] = [
   },
   {
     title: "Memory Game",
-    details: "A memory game front end project made with React.",
+    details: "A memory game front-end application, made with React.",
     image: memory,
     link: "https://memory-card-game-umber.vercel.app/",
-    list: ["Implemented <strong>Zustand</strong> for state management."],
+    list: [
+      "Learned how to use <strong>React hooks</strong> and <strong>lifecycle</strong> methods.",
+      "Implemented <strong>Zustand</strong> for state management.",
+    ],
     tags: [
       { name: "Tailwind", color: "emerald" },
       { name: "React", color: "blue" },
       { name: "Zustand", color: "red" },
     ],
-  },
-  {
-    title: "Memory Game",
-    details: "A memory game front end project made with React.",
-    image: memory,
-    link: "https://memory-card-game-umber.vercel.app/",
-  },
-  {
-    title: "Memory Game",
-    details: "A memory game front end project made with React.",
-    image: memory,
-    link: "https://memory-card-game-umber.vercel.app/",
   },
 ];
 
