9c2f71b538feddd02c6c717046eba2b566a00de5
Cake <cakemancerr@gmail.com>
2023-01-01 23:38:14 +0100

    feat: add i18next for translations

---
+++ astro-i18next.config.ts
@@ -0,0 +1,11 @@
+/** @type {import('astro-i18next').AstroI18nextConfig} */
+export default {
+  defaultLocale: "en",
+  locales: ["en", "fr"],
+  routes: {
+    fr: {
+      about: "a-propos",
+      projects: "projets",
+    },
+  },
+};
--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from "astro/config";
 import compress from "astro-compress";
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
+import astroI18next from "astro-i18next";
 
-// https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig({
   integrations: [
+    astroI18next(),
     compress(),
     tailwind({
       config: {
