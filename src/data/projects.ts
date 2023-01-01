165a07e2ccd471a93b8bc65ba16244be7b1dfaf6
Cake <cakemancerr@gmail.com>
2023-01-02 00:28:41 +0100

    feat: add translation to french!

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -2,40 +2,34 @@ import shopping from "/shopping.webp";
 import memory from "/memory.webp";
 import blog from "/blog.webp";
 import type { ProjectProps } from "@customTypes/projects";
+import { t } from "i18next";
 
 export const projectsData: ProjectProps[] = [
   {
-    title: "Blog API",
-    details:
-      "A full-stack blog application made with Next JS for the front-end and Express JS for the back-end.",
+    title: "projects.blog.title",
+    details: "projects.blog.details",
     image: blog,
     link: "https://blog-cakegod.vercel.app/blog",
-    list: [
-      "Created a <strong>REST API</strong> as the back-end with Express JS.",
-      "Implemented <strong>MongoDB</strong> as the database.",
-    ],
+    list: ["projects.blog.list.0", "projects.blog.list.1"],
     tags: ["Tailwind", "Zustand", "MongoDB", "Express"],
   },
   {
-    title: "Shopping Cart",
-    details:
-      "An e-commerce front-end application, made with React and Next JS.",
+    title: "projects.shopping.title",
+    details: "projects.shopping.details",
     image: shopping,
     link: "https://shopping-cart-cakegod.vercel.app/shop",
     list: [
-      "Utilized <strong>Next.js</strong> for routing and optimization of images and fonts.",
-      "Implemented <strong>React context</strong> to avoid prop drilling.",
+			"projects.shopping.list.0", "projects.shopping.list.1"
     ],
     tags: ["Tailwind", "React", "Next JS"],
   },
   {
-    title: "Memory Game",
-    details: "A memory game front-end application, made with React.",
+    title: "projects.memory.title",
+    details: "projects.memory.details",
     image: memory,
-    link: "https://memory-card-game-umber.vercel.app/",
+    link: "https://memory-card-umber.vercel.app/",
     list: [
-      "Learned how to use <strong>React hooks</strong> and <strong>lifecycle</strong> methods.",
-      "Implemented <strong>Zustand</strong> for state management.",
+      "projects.memory.list.0", "projects.memory.list.1"
     ],
     tags: ["Tailwind", "React", "Zustand"],
   },
