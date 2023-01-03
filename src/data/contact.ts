ce5db37a21489e99c8591bdd590d843b46f134bb
Cake <cakemancerr@gmail.com>
2023-01-04 00:11:24 +0100

    feat: add more contacts

---
+++ src/data/contact.ts
@@ -0,0 +1,16 @@
+import type { ContactProps } from "@customTypes/contacts";
+
+export const contactMethods: ContactProps[] = [
+  {
+    heading: "contact.email.heading",
+    description: "contact.email.description",
+    button: "contact.email.button",
+    link: "contact.email.link",
+  },
+  {
+    heading: "contact.github.heading",
+    description: "contact.github.description",
+    button: "contact.github.button",
+    link: "contact.github.link",
+  },
+];
