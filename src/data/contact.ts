19fce8700b334bc005c1e11cad237bfac6f23bb7
Cake <cakemancerr@gmail.com>
2023-01-04 21:11:10 +0100

    refactor: compose

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -2,13 +2,13 @@ import type { ContactProps } from "@customTypes/contacts";
 
 export const contactMethods: ContactProps[] = [
   {
-    heading: "contact.email.heading",
+    heading: "E-mail",
     description: "contact.email.description",
     button: "contact.email.button",
     link: "contact.email.link",
   },
   {
-    heading: "contact.github.heading",
+    heading: "Github",
     description: "contact.github.description",
     button: "contact.github.button",
     link: "contact.github.link",
