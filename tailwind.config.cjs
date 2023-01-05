dad77b5b59155f499dc6d802624f3b278835bfaf
Cake <cakemancerr@gmail.com>
2023-01-05 17:28:06 +0100

    refactor: menu animation

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,16 +9,6 @@ module.exports = {
         sans: ["InterVariable", ...defaultTheme.fontFamily.sans],
       },
       keyframes: {
-        menu: {
-          "0%": { transform: "scaleX(0)" },
-          "50%": { transform: "scaleX(1.2)" },
-          "100%": { transform: "scaleX(1)" },
-        },
-        "menu-reverse": {
-          "0%": { transform: "scaleX(1)" },
-          "50%": { transform: "scaleX(1.2)" },
-          "100%": { transform: "scaleX(0)" },
-        },
         text: {
           "0%, 100%": {
             "background-size": "200% 200%",
@@ -31,8 +21,6 @@ module.exports = {
         },
       },
       animation: {
-        open: "menu 0.4s ease forwards",
-        "open-reverse": "menu-reverse 0.4s ease forwards",
         text: "text 5s ease infinite",
       },
     },
